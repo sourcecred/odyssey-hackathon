229ca087d1dadcbd739241ae87c05092f9ac7c47
Dandelion Mané <decentralion@dandelion.io>
2019-04-12 21:14:51 -0400

    Integrate some trivial D3 code

--- src/graphviz/App.js
+++ src/graphviz/App.js
@@ -7,6 +7,7 @@ import type {LocalStore} from "../webutil/localStore";
 import CheckedLocalStore from "../webutil/checkedLocalStore";
 import BrowserLocalStore from "../webutil/browserLocalStore";
 import Link from "../webutil/Link";
+import * as d3 from "d3";
 
 export class AppPage extends React.Component<{|
   +assets: Assets,
@@ -19,12 +20,39 @@ export class AppPage extends React.Component<{|
   );
 
   render() {
-    return <GraphViz />;
+    return <GraphViz _rootNode={null} />;
   }
 }
 
 export class GraphViz extends React.Component<{}> {
+  _rootNode: HTMLDivElement | null;
+
+  componentDidMount() {
+    const svg = d3
+      .select(this._rootNode)
+      .append("svg")
+      .attr("width", 700)
+      .attr("height", 700);
+    svg
+      .append("circle")
+      .attr("cx", 20)
+      .attr("cy", 30)
+      .attr("r", 3);
+  }
+
+  componentDidUpdate() {
+    return;
+  }
+
+  componentWillUnmount() {
+    return;
+  }
+
+  _setRef(componentNode: HTMLDivElement | null) {
+    this._rootNode = componentNode;
+  }
+
   render() {
-    return <p>Hello. I declare myself to be GraphViz. </p>;
+    return <div className="graph-container" ref={this._setRef.bind(this)} />;
   }
 }
