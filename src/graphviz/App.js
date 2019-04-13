e9dc0840d5598d45badd1bf7e9d111bfad5eda35
Dandelion Mané <decentralion@dandelion.io>
2019-04-13 17:00:20 -0400

    d3 graph can now handle updates
    it's still pretty gnarly though

--- src/graphviz/App.js
+++ src/graphviz/App.js
@@ -18,18 +18,15 @@ import {PagerankGraph} from "../core/pagerankGraph";
 import * as NullUtil from "../util/null";
 
 const BACKGROUND_COLOR = "#313131";
-const INTERPOLATE_LOW = "#00ABE1";
-const INTERPOLATE_HIGH = "#90FF03";
 const EDGE_COLOR = "#111111";
 
-// Orange for prioity
-const PRIORITY_COLORS = ["#FFD3A0", "#FF8B03"];
+const COLORS_BY_TYPE = {
+  PERSON: ["#C4E2FF", "#2D88DC"],
+  CONTRIBUTION: ["#FEBAFF", "#8600C6"],
+  PRIORITY: ["#FFD3A0", "#FF8B03"],
+};
 
-// Blue (purple?)
-const PERSON_COLORS = ["#C4E2FF", "#2D88DC"];
-
-// Green
-const CONTRIBUTION_COLORS = ["#FEBAFF", "#8600C6"];
+const MAX_SIZE_PIXELS = 200;
 
 export class AppPage extends React.Component<{|
   +assets: Assets,
@@ -79,11 +76,14 @@ export class OdysseyViz extends React.Component<
       maxIterations: 100,
       convergenceThreshold: 1e-3,
     });
-    this.setState({nodes: this._computeNodes()});
+    const nodes = this._computeNodes();
+    this.setState({nodes});
   }
 
   render() {
-    return <GraphViz nodes={this.state.nodes} edges={this.state.edges} />;
+    const nodes = this.state.nodes;
+    const edges = this.state.edges;
+    return <GraphViz nodes={nodes} edges={edges} />;
   }
 }
 
@@ -102,23 +102,51 @@ export type GraphVizProps = {|
 
 // For graph visualization: inspiration and code from Ryan Morton:
 // https://discourse.sourcecred.io/t/research-design-exploratory-data-analysis/67
-// For React + D3 integration: based on work by Nicolas Hery
-// http://nicolashery.com/integrating-d3js-visualizations-in-a-react-app/
+// For React integration: this is a hacky mess based on the first approach described
+// in this blog post: https://www.smashingmagazine.com/2018/02/react-d3-ecosystem/
 export class GraphViz extends React.Component<GraphVizProps> {
   _rootNode: HTMLDivElement | null;
   simulation: any;
+  _maxScore: number;
 
-  async componentDidMount() {
+  _chart: any;
+  _nodesG: any;
+  _edgesG: any;
+  _textG: any;
+  _tooltip: any;
+  _mouseOver: any;
+  _mouseOff: any;
+  _ticked: any;
+
+  _computeMax() {
+    this._maxScore = -Infinity;
+    for (const {score, type} of this.props.nodes) {
+      if (score > this._maxScore) {
+        this._maxScore = score;
+      }
+    }
+  }
+
+  _color(d: ScoredEntity) {
+    const scoreRatio = d.score / this._maxScore;
+    const colorEndpoints = COLORS_BY_TYPE[d.type];
+    return d3.interpolate(...colorEndpoints)(scoreRatio);
+  }
+
+  _radius(d: ScoredEntity) {
+    return Math.sqrt((d.score / this._maxScore) * MAX_SIZE_PIXELS);
+  }
+
+  _setupScaffold() {
     const svg = d3
       .select(this._rootNode)
       .append("svg")
       .style("flex-grow", 1);
     const rect = svg.node().getBoundingClientRect();
-    const chart = svg
+    this._chart = svg
       .append("g")
       .attr("transform", `translate(${rect.width / 2}, ${rect.height / 2})`);
-
-    const tooltip = d3
+    this._tooltip = d3
       .select(this._rootNode)
       .append("div")
       .attr("class", "toolTip")
@@ -128,107 +156,73 @@ export class GraphViz extends React.Component<GraphVizProps> {
       .style("height", "auto")
       .style("background", "none repeat scroll 0 0 #ffffff");
 
-    const edgesG = chart.append("g");
-    const nodesG = chart.append("g");
-    const textG = chart.append("g");
+    this._edgesG = this._chart.append("g");
+    this._nodesG = this._chart.append("g");
+    this._textG = this._chart.append("g");
 
-    function colorFor(d: ScoredEntity) {
-      const maxForType = maxByType[d.type];
-      const scoreRatio = d.score / maxScore;
-      switch (d.type) {
-        case "PERSON":
-          return d3.interpolate(PERSON_COLORS[0], PERSON_COLORS[1])(scoreRatio);
-        case "CONTRIBUTION":
-          return d3.interpolate(CONTRIBUTION_COLORS[0], CONTRIBUTION_COLORS[1])(
-            scoreRatio
-          );
-        case "PRIORITY":
-          return d3.interpolate(PRIORITY_COLORS[0], PRIORITY_COLORS[1])(
-            scoreRatio
-          );
-        default:
-          throw new Error("Unrecognized type.");
-      }
-    }
-
-    function mouseOver() {
+    const that = this;
+    this._mouseOver = function() {
       var data = d3.select(this).data()[0];
       var textDisplay = data.score;
 
-      tooltip
+      that._tooltip
         .style("left", d3.event.pageX - 10 + "px")
         .style("top", d3.event.pageY - 30 + "px")
         .style("display", "inline-block")
-        .html(function() {
+        .html(() => {
           return textDisplay;
         });
-    }
-
-    function mouseOff() {
-      tooltip.style("display", "none");
-    }
-
-    let maxScore = -Infinity;
-    const maxByType = {
-      PERSON: -Infinity,
-      CONTRIBUTION: -Infinity,
-      PRIORITY: -Infinity,
     };
 
-    for (const {score, type} of this.props.nodes) {
-      if (score > maxScore) {
-        maxScore = score;
-      }
-      if (score > maxByType[type]) {
-        maxByType[type] = score;
-      }
-    }
+    this._mouseOff = () => {
+      this._tooltip.style("display", "none");
+    };
 
+    this._ticked = () => {
+      this._nodesG
+        .selectAll(".node")
+        .attr("cx", (d) => {
+          return d.x;
+        })
+        .attr("cy", (d) => {
+          return d.y;
+        });
+
+      this._textG
+        .selectAll(".text")
+        .attr("x", (d) => {
+          return d.x + this._radius(d) + 5;
+        })
+        .attr("y", (d) => {
+          return d.y + 5;
+        });
+
+      //TODO: fix arrow marker by moving back based on the node radius
+      this._edgesG
+        .selectAll(".edge")
+        .attr("x1", (d) => {
+          return d.source.x;
+        })
+        .attr("y1", (d) => {
+          return d.source.y;
+        })
+        .attr("x2", (d) => {
+          return d.target.x;
+        })
+        .attr("y2", (d) => {
+          return d.target.y;
+        });
+    };
+  }
+
+  _updateD3() {
+    this._computeMax();
     const links = this.props.edges.map((e) => ({
       source: e.src,
       target: e.dst,
       address: e.address,
     }));
 
-    function radius(d: ScoredEntity) {
-      return Math.sqrt((d.score / maxScore) * 200);
-    }
-
-    const ticked = () => {
-      nodesG
-        .selectAll(".node")
-        .attr("cx", function(d) {
-          return d.x;
-        })
-        .attr("cy", function(d) {
-          return d.y;
-        });
-
-      textG
-        .selectAll(".text")
-        .attr("x", function(d) {
-          return d.x + radius(d) + 5;
-        })
-        .attr("y", function(d) {
-          return d.y + 5;
-        });
-
-      //TODO: fix arrow marker by moving back based on the node radius
-      edgesG
-        .selectAll(".edge")
-        .attr("x1", function(d) {
-          return d.source.x;
-        })
-        .attr("y1", function(d) {
-          return d.source.y;
-        })
-        .attr("x2", function(d) {
-          return d.target.x;
-        })
-        .attr("y2", function(d) {
-          return d.target.y;
-        });
-    };
     this.simulation = d3
       .forceSimulation(this.props.nodes)
       .force("charge", d3.forceManyBody().strength(-380))
@@ -242,15 +236,17 @@ export class GraphViz extends React.Component<GraphVizProps> {
       )
       .force(
         "collide",
-        d3.forceCollide().radius(function(d) {
-          return radius(d) * 2;
+        d3.forceCollide().radius((d) => {
+          return this._radius(d) * 2;
         })
       )
       .force("x", d3.forceX())
       .force("y", d3.forceY())
-      .on("tick", ticked);
+      .on("tick", this._ticked);
 
-    const nodeSelection = nodesG.selectAll(".node").data(this.props.nodes);
+    const nodeSelection = this._nodesG
+      .selectAll(".node")
+      .data(this.props.nodes);
     nodeSelection
       .exit()
       .transition()
@@ -261,18 +257,18 @@ export class GraphViz extends React.Component<GraphVizProps> {
       .enter()
       .append("circle")
       .attr("class", "node")
-      .on("mouseover", mouseOver)
-      .on("mouseout", mouseOff);
+      .on("mouseover", this._mouseOver)
+      .on("mouseout", this._mouseOff);
 
     nodeSelection
       .merge(newNodes)
       .transition()
       .ease(d3.easeQuad)
       .duration(1000)
-      .attr("fill", colorFor)
-      .attr("r", radius);
+      .attr("fill", this._color.bind(this))
+      .attr("r", this._radius.bind(this));
 
-    const textSelection = textG.selectAll(".text").data(this.props.nodes);
+    const textSelection = this._textG.selectAll(".text").data(this.props.nodes);
     textSelection
       .exit()
       .transition()
@@ -289,14 +285,14 @@ export class GraphViz extends React.Component<GraphVizProps> {
       .transition()
       .ease(d3.easeQuad)
       .duration(1000)
-      .text(function(d) {
+      .text((d) => {
         return Math.floor(d.score * 1000);
       })
-      .attr("fill", colorFor)
+      .attr("fill", this._color.bind(this))
       .attr("font-size", 14);
 
     // edge data join
-    var edge = edgesG.selectAll(".edge").data(links);
+    var edge = this._edgesG.selectAll(".edge").data(links);
 
     // edge exit
     edge.exit().remove();
@@ -313,21 +309,22 @@ export class GraphViz extends React.Component<GraphVizProps> {
       .ease(d3.easeQuad)
       .duration(1000)
       .attr("marker-end", "url(#arrow)")
-      .attr("stroke-width", function(d) {
+      .attr("stroke-width", () => {
         return "1px";
       })
       .attr("opacity", "0.4")
-      .attr("stroke", function(d) {
+      .attr("stroke", () => {
         return EDGE_COLOR;
       });
   }
 
-  componentDidUpdate() {
-    return;
+  componentDidMount() {
+    this._setupScaffold();
+    this._updateD3();
   }
 
-  componentWillUnmount() {
-    return;
+  componentDidUpdate() {
+    this._updateD3();
   }
 
   _setRef(componentNode: HTMLDivElement | null) {
